557b5fd2b287c8b65d73294d839ab8650ca0425f
fanlu <fanlu@jd.com>
2018-11-14 17:01:41 +0800

    add kefu to v3.0

---
+++ egs/kefu/asr1/run.sh
@@ -0,0 +1,332 @@
+#!/bin/bash
+
+# Copyright 2017 Johns Hopkins University (Shinji Watanabe)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh
+. ./cmd.sh
+n_iter_processes=24
+queue=other.q
+host=GPU_172_28_230_53
+# general configuration
+backend=pytorch
+stage=0        # start from 0 if you need to start from data preparation
+ngpu=1         # number of gpus ("0" uses cpu, otherwise use gpu)
+debugmode=1
+dumpdir=dump   # directory to dump full features
+N=0            # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
+verbose=0      # verbose option
+resume=        # Resume the training from snapshot
+
+# feature configuration
+do_delta=false
+
+# network archtecture
+# encoder related
+etype=vggblstm     # encoder architecture type
+elayers=3
+eunits=1024
+eprojs=1024
+subsample=1_2_2_1_1 # skip every n frame from input to nth layers
+# decoder related
+dlayers=2
+dunits=1024
+# attention related
+atype=location
+adim=1024
+aconv_chans=10
+aconv_filts=100
+
+# hybrid CTC/attention
+mtlalpha=0.5
+
+# minibatch related
+batchsize=30
+maxlen_in=800  # if input length  > maxlen_in, batchsize is automatically reduced
+maxlen_out=150 # if output length > maxlen_out, batchsize is automatically reduced
+
+# optimization related
+opt=adadelta
+epochs=25
+
+# rnnlm related
+lm_layers=2
+lm_units=650
+lm_opt=sgd        # or adam
+lm_batchsize=64   # batch size in LM training
+lm_epochs=20      # if the data size is large, we can reduce this
+lm_maxlen=100     # if sentence length > lm_maxlen, lm_batchsize is automatically reduced
+lm_resume=        # specify a snapshot file to resume LM training
+lmtag=            # tag for managing LMs
+
+# decoding parameter
+lm_weight=0.3
+beam_size=20
+penalty=0.0
+maxlenratio=0.0
+minlenratio=0.0
+ctc_weight=0.6
+recog_model=model.acc.best # set a model to be used for decoding: 'model.acc.best' or 'model.loss.best'
+
+# scheduled sampling option
+samp_prob=0.0
+
+# data
+hkust1=/export/corpora/LDC/LDC2005S15/
+hkust2=/export/corpora/LDC/LDC2005T32/
+
+# exp tag
+tag="" # tag for managing experiments.
+
+
+. ./path.sh
+. ./cmd.sh
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+#train_set=train_nodup_sp
+train_dev=train_dev
+#recog_set="dev train_dev"
+train_set=train_nodev
+#train_dev=dev
+#recog_set="test_kefu"
+recog_set="kefu_eval_200h kefu_test3"
+echo $stage
+data=/mnt/cephfs2/asr/database/AM/aishell/
+data=/opt/cephfs1/asr/users/fanlu/mfs/aishell/
+. utils/parse_options.sh || exit 1;
+if [ ${stage} -le 0 ]; then
+    # Lexicon Preparation,
+    local/aishell_prepare_dict.sh $data/resource_aishell || exit 1;
+
+    # Data Preparation,
+    local/aishell_data_prep.sh $data/data_aishell/wav $data/data_aishell/transcript || exit 1;
+
+    # Phone Sets, questions, L compilation
+    utils/prepare_lang.sh --position-dependent-phones false data/local/dict \
+            "<SPOKEN_NOISE>" data/local/lang data/lang || exit 1;
+
+    # LM training
+    local/aishell_train_lms.sh || exit 1;
+
+    # G compilation, check LG composition
+    utils/format_lm.sh data/lang data/local/lm/3gram-mincount/lm_unpruned.gz \
+            data/local/dict/lexicon.txt data/lang_test || exit 1;
+fi
+
+feat_tr_dir=${dumpdir}/${train_set}/delta${do_delta}; mkdir -p ${feat_tr_dir}
+feat_dt_dir=${dumpdir}/${train_dev}/delta${do_delta}; mkdir -p ${feat_dt_dir}
+echo $feat_tr_dir $feat_dt_dir
+if [ ${stage} -le 1 ]; then
+    ### Task dependent. You have to design training and dev sets by yourself.
+    ### But you can utilize Kaldi recipes in most cases
+    echo "stage 1: Feature Generation"
+    fbankdir=fbank
+    ## Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
+    #steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 400 --write_utt2num_frames true \
+    #    data/train exp/make_fbank/train ${fbankdir}
+    ##steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 10 --write_utt2num_frames true \
+    ##    data/dev exp/make_fbank/dev ${fbankdir}
+    #steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 100 --write_utt2num_frames true \
+    #    data/test_kefu exp/make_fbank/test_kefu ${fbankdir}
+
+    ## make a dev set
+    #utils/subset_data_dir.sh --first data/train 4000 data/${train_dev}
+    #utils/fix_data_dir.sh data/${train_dev}
+    #n=$[`cat data/train/wav.scp | wc -l` - 4000]
+    #utils/subset_data_dir.sh --last data/train $n data/train_nodev
+
+    ### make a training set
+    ##utils/data/remove_dup_utts.sh 300 data/train_nodev data/train_nodup
+
+    ### speed-perturbed
+    ##utils/perturb_data_dir_speed.sh 0.9 data/train_nodup data/temp1
+    ##utils/perturb_data_dir_speed.sh 1.0 data/train_nodup data/temp2
+    ##utils/perturb_data_dir_speed.sh 1.1 data/train_nodup data/temp3
+    ##utils/combine_data.sh --extra-files utt2uniq data/${train_set} data/temp1 data/temp2 data/temp3
+    ##rm -r data/temp1 data/temp2 data/temp3
+    ##steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 32 --write_utt2num_frames true \
+    ##    data/${train_set} exp/make_fbank/${train_set} ${fbankdir}
+
+    ## compute global CMVN
+    #compute-cmvn-stats scp:data/${train_set}/feats.scp data/${train_set}/cmvn.ark
+
+    ## dump features for training
+    #split_dir=`echo $PWD | awk -F "/" '{print $NF "/" $(NF-1)}'`
+    #if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_tr_dir}/storage ]; then
+    #utils/create_split_dir.pl \
+    #    /export/a{11,12,13,14}/${USER}/espnet-data/egs/${split_dir}/dump/${train_set}/delta${do_delta}/storage \
+    #    ${feat_tr_dir}/storage
+    #fi
+    #if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_dt_dir}/storage ]; then
+    #utils/create_split_dir.pl \
+    #    /export/a{11,12,13,14}/${USER}/espnet-data/egs/${split_dir}/dump/${train_dev}/delta${do_delta}/storage \
+    #    ${feat_dt_dir}/storage
+    #fi
+    #dump.sh --cmd "$train_cmd" --nj 400 --do_delta $do_delta \
+    #    data/${train_set}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/train ${feat_tr_dir}
+    #dump.sh --cmd "$train_cmd" --nj 100 --do_delta $do_delta \
+    #    data/${train_dev}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
+    for rtask in ${recog_set}; do
+    	steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 100 --write_utt2num_frames true \
+    	    data/${rtask} exp/make_fbank/${rtask} ${fbankdir}
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
+        dump.sh --cmd "$train_cmd" --nj 100 --do_delta $do_delta \
+            data/${rtask}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
+            ${feat_recog_dir}
+    done
+fi
+
+dict=data/lang_1char/${train_set}_units.txt
+echo "dictionary: ${dict}"
+nlsyms=data/lang_1char/non_lang_syms.txt
+if [ ${stage} -le 2 ]; then
+    ### Task dependent. You have to check non-linguistic symbols used in the corpus.
+    echo "stage 2: Dictionary and Json Data Preparation"
+    mkdir -p data/lang_1char/
+
+    #echo "make a non-linguistic symbol list"
+    #cut -f 2- data/${train_set}/text | grep -o -P '\[.*?\]' | sort | uniq > ${nlsyms}
+    #cat ${nlsyms}
+
+    echo "make a dictionary"
+    echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
+    text2token.py -s 1 -n 1 data/${train_set}/text | cut -f 2- -d" " | tr " " "\n" \
+    | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' >> ${dict}
+    wc -l ${dict}
+
+    echo "make json files"
+    data2json.sh --feat ${feat_tr_dir}/feats.scp --nlsyms ${nlsyms} \
+         data/${train_set} ${dict} > ${feat_tr_dir}/data.json
+    data2json.sh --feat ${feat_dt_dir}/feats.scp --nlsyms ${nlsyms} \
+         data/${train_dev} ${dict} > ${feat_dt_dir}/data.json
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+        data2json.sh --feat ${feat_recog_dir}/feats.scp \
+            --nlsyms ${nlsyms} data/${rtask} ${dict} > ${feat_recog_dir}/data.json
+    done
+fi
+# you can skip this and remove --rnnlm option in the recognition (stage 5)
+if [ -z ${lmtag} ]; then
+    lmtag=${lm_layers}layer_unit${lm_units}_${lm_opt}_bs${lm_batchsize}
+fi
+lmexpdir=exp/train_rnnlm_${backend}_${lmtag}
+mkdir -p ${lmexpdir}
+
+if [ ${stage} -le 3 ]; then
+    echo "stage 3: LM Preparation"
+    lmdatadir=data/local/lm_train
+    mkdir -p ${lmdatadir}
+    text2token.py -s 1 -n 1 -l ${nlsyms} data/train/text | cut -f 2- -d" " \
+        > ${lmdatadir}/train.txt
+    text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_dev}/text | cut -f 2- -d" " \
+        > ${lmdatadir}/valid.txt
+    # use only 1 gpu
+    if [ ${ngpu} -gt 1 ]; then
+        echo "LM training does not support multi-gpu. signle gpu will be used."
+    fi
+    ${cuda_cmd} --gpu ${ngpu} ${lmexpdir}/train.log bash /mnt/cephfs2/asr/users/fanlu/espnet/tools/run_mgpu.sh ${ngpu} lm_train.py \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --verbose 1 \
+        --outdir ${lmexpdir} \
+        --train-label ${lmdatadir}/train.txt \
+        --valid-label ${lmdatadir}/valid.txt \
+        --resume ${lm_resume} \
+        --layer ${lm_layers} \
+        --unit ${lm_units} \
+        --opt ${lm_opt} \
+        --batchsize ${lm_batchsize} \
+        --epoch ${lm_epochs} \
+        --maxlen ${lm_maxlen} \
+        --dict ${dict}
+fi
+
+if [ -z ${tag} ]; then
+    expdir=exp/${train_set}_${backend}_${etype}_e${elayers}_subsample${subsample}_unit${eunits}_proj${eprojs}_d${dlayers}_unit${dunits}_${atype}_aconvc${aconv_chans}_aconvf${aconv_filts}_mtlalpha${mtlalpha}_${opt}_sampprob${samp_prob}_bs${batchsize}_mli${maxlen_in}_mlo${maxlen_out}
+    if ${do_delta}; then
+        expdir=${expdir}_delta
+    fi
+else
+    expdir=exp/${train_set}_${backend}_${tag}
+fi
+mkdir -p ${expdir}
+
+if [ ${stage} -le 4 ]; then
+    echo "stage 4: Network Training"
+    ${cuda_cmd} --gpu ${ngpu} -q ${queue} -l h="${host}" ${expdir}/train.log bash /mnt/cephfs2/asr/users/fanlu/run_mgpu.sh ${ngpu} asr_train.py \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --outdir ${expdir}/results \
+        --debugmode ${debugmode} \
+        --dict ${dict} \
+        --debugdir ${expdir} \
+        --minibatches ${N} \
+        --verbose ${verbose} \
+        --resume ${resume} \
+        --train-json ${feat_tr_dir}/data.json \
+        --valid-json ${feat_dt_dir}/data.json \
+        --etype ${etype} \
+        --n_iter_processes ${n_iter_processes} \
+        --elayers ${elayers} \
+        --eunits ${eunits} \
+        --eprojs ${eprojs} \
+        --subsample ${subsample} \
+        --dlayers ${dlayers} \
+        --dunits ${dunits} \
+        --atype ${atype} \
+        --adim ${adim} \
+        --aconv-chans ${aconv_chans} \
+        --aconv-filts ${aconv_filts} \
+        --mtlalpha ${mtlalpha} \
+        --batch-size ${batchsize} \
+        --maxlen-in ${maxlen_in} \
+        --maxlen-out ${maxlen_out} \
+        --sampling-probability ${samp_prob} \
+        --opt ${opt} \
+        --epochs ${epochs}
+fi
+if [ ${stage} -le 5 ]; then
+    echo "stage 5: Decoding"
+    nj=200
+
+    for rtask in ${recog_set}; do
+    (
+        decode_dir=decode_${rtask}_beam${beam_size}_e${recog_model}_p${penalty}_len${minlenratio}-${maxlenratio}_ctcw${ctc_weight}_rnnlm${lm_weight}_${lmtag}
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+
+        # split data
+        splitjson.py --parts ${nj} ${feat_recog_dir}/data.json
+	echo ${feat_recog_dir}
+        #### use CPU for decoding
+        ngpu=0
+
+        # ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json --result-label ${expdir}/${decode_dir}/data.JOB.json 
+        #${decode_cmd} --gpu ${ngpu} ${expdir}/${decode_dir}/log/decode.log bash /mnt/cephfs2/asr/users/fanlu/espnet/tools/run_mgpu.sh ${ngpu} asr_recog.py \
+        ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log asr_recog.py \
+            --ngpu ${ngpu} \
+            --backend ${backend} \
+            --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json \
+            --result-label ${expdir}/${decode_dir}/data.JOB.json \
+            --model ${expdir}/results/${recog_model}  \
+            --beam-size ${beam_size} \
+            --penalty ${penalty} \
+            --maxlenratio ${maxlenratio} \
+            --minlenratio ${minlenratio} \
+            --ctc-weight ${ctc_weight} \
+            --rnnlm ${lmexpdir}/rnnlm.model.best \
+            --lm-weight ${lm_weight} &
+        wait
+
+        score_sclite.sh --nlsyms ${nlsyms} ${expdir}/${decode_dir} ${dict}
+
+    ) &
+    done
+    wait
+    echo "Finished"
+fi
+
